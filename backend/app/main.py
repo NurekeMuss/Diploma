b8c53f47dda62e1adc595110cbe653bbc1afa754
Damir Ospanbek <ospanbekdamir1@gmail.com>
2025-02-22 21:24:39 +0500

    back

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.base_router import router as base_router
+from app.autoReport.reportGenerator import router as report_router  # Добавил новый роутер
 
 app = FastAPI(title="My FastAPI Project")
 
-# Configure CORS
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],  # Allows the Next.js dev server
+    allow_origins=["http://localhost:3000", "*"],  # Для Next.js
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+# Подключение роутеров
 app.include_router(base_router)
+app.include_router(report_router)  # Подключаем эндпоинты из reportGenerator.py
